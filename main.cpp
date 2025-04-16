ee95496014dca007f83ea961596900725ec58192
qxyliy <1066976880@qq.com>
2025-04-17 03:12:18 +0800

    前端UI接口

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@ int main() {
     cout << "This is a test program." << endl;
     cout << "It will not do anything useful." << endl;
     cout << "But it will compile and run." << endl;
+    //cout << "testest" << endl;
     
     return 0;
 } // Yunyao will always wait for Akiha.
