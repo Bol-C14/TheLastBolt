28bfa50decef98499f84fb934b846538bf39bf3a
lxyaa1 <117246711+lxyaa1@users.noreply.github.com>
2025-04-17 23:23:59 +0800

    enemyAI

---
+++ src/EnemyAI.cpp
@@ -0,0 +1,21 @@
+#include"EnemyAI.h"
+
+void EnemyState::Enter(){
+
+}
+void EnemyState::Exit(){
+    
+}
+void EnemyStateMachine:: Initialize(EnemyState state){
+
+}
+void EnemyStateMachine::ChangeState(EnemyState nextState){
+    
+}
+
+void EnemyAI::UpdateAI(Enemy &enemy, BattleState &battle){
+
+}
+ActionType EnemyAI:: GetNextAction(const Enemy &enemy){
+
+}
