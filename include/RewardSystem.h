44be6c1159f5792c97e6e409a1457adbbb5821fa
Shenrella <2782945353@qq.com>
2025-04-18 21:15:04 +0800

    更改为Allman风格

--- include/RewardSystem.h
+++ include/RewardSystem.h
@@ -1,23 +1,31 @@
 /*
-	������		����ɡ
-	����		��������ҽ��������н����п��ơ��ָ�����ֵ������
-	����ýӿ�	��
-	�����ýӿ�	��
+	负责人				：沈伞
+	功能				：结算
+	需调用函数			：
+	被调用函数			：
 */
+
+
 
 #ifndef REWARDSYSTEM_H
 #define REWARDSYSTEM_H
 
-#include "CommonTypes.h"
 #include <vector>
 #include <string>
 
+#include "CommonTypes.h"
+
+
+
 // 事件类型，用于区分奖励来源
-enum class RewardEventType {
-    VICTORY,
-    BOSS_DEFEAT,
-    FAILURE
+enum class RewardEventType
+{
+	VICTORY,
+	BOSS_DEFEAT,
+	FAILURE
 };
+
+
 
 /**
  * 获取指定事件的奖励列表
@@ -26,6 +34,8 @@ enum class RewardEventType {
  */
 std::vector<RewardOption> RewardSystem_GetRewardsForEvent(RewardEventType eventType);
 
+
+
 /**
  * 应用选中的奖励到玩家状态
  * @param reward 奖励选项
@@ -33,10 +43,14 @@ std::vector<RewardOption> RewardSystem_GetRewardsForEvent(RewardEventType eventT
  */
 void RewardSystem_ApplyReward(const RewardOption &reward, PlayerState &player);
 
+
+
 /**
  * 随机生成一个卡牌奖励
  * @return 奖励选项
  */
 RewardOption RewardSystem_GenerateRandomCardReward();
+
+
 
 #endif // REWARDSYSTEM_H
--- src/RewardSystem.cpp
+++ src/RewardSystem.cpp
@@ -1,67 +1,93 @@
-#include "../include/RewardSystem.h"
 #include <nlohmann/json.hpp>
 #include <fstream>
 #include <iostream>
 #include <random>
 
+#include "../include/RewardSystem.h"
+
 using json = nlohmann::json;
 
+
+
 // 辅助：将字符串映射到事件类型
-static std::string EventTypeToString(RewardEventType evt) {
-    switch (evt) {
+static std::string EventTypeToString(RewardEventType evt) 
+{
+    switch (evt) 
+    {
         case RewardEventType::VICTORY:     return "VICTORY";
+
         case RewardEventType::BOSS_DEFEAT:  return "BOSS_DEFEAT";
+
         case RewardEventType::FAILURE:      return "FAILURE";
     }
     return "";
 }
 
-std::vector<RewardOption> RewardSystem_GetRewardsForEvent(RewardEventType eventType) {
+
+
+std::vector<RewardOption> RewardSystem_GetRewardsForEvent(RewardEventType eventType) 
+{
     std::vector<RewardOption> result;
     std::ifstream file("data/rewards.json");
-    if (!file.is_open()) {
+
+    if (!file.is_open()) 
+    {
         std::cerr << "无法打开奖励文件 data/rewards.json" << std::endl;
         return result;
     }
-    try {
+    try 
+    {
         json j; file >> j;
         std::string key = EventTypeToString(eventType);
-        if (!j.contains(key)) {
+        if (!j.contains(key)) 
+        {
             std::cerr << "奖励JSON中未找到事件: " << key << std::endl;
             return result;
         }
-        for (auto &item : j[key]) {
+        for (auto &item : j[key]) 
+        {
             RewardOption opt;
             opt.type = item.at("type").get<RewardType>();
             opt.value = item.at("value").get<int>();
             opt.description = item.at("description").get<std::string>();
             result.push_back(opt);
         }
-    } catch (const std::exception &e) {
+    } catch (const std::exception &e) 
+    {
         std::cerr << "解析奖励JSON出错: " << e.what() << std::endl;
     }
+
     return result;
 }
 
-void RewardSystem_ApplyReward(const RewardOption &reward, PlayerState &player) {
-    switch (reward.type) {
+
+
+void RewardSystem_ApplyReward(const RewardOption &reward, PlayerState &player) 
+{
+    switch (reward.type) 
+    {
         case RewardType::HEAL:
             player.hp = std::min(player.maxHp, player.hp + reward.value);
             std::cout << "获得回血 " << reward.value << " 点，当前HP " << player.hp << std::endl;
             break;
+
         case RewardType::CARD:
             player.deck.push_back(reward.value);
             std::cout << "获得卡牌ID " << reward.value << std::endl;
             break;
+
         case RewardType::SKILLPOINT:
             player.skillPoints += reward.value;
             std::cout << "获得技能点 " << reward.value << ", 当前技能点 " << player.skillPoints << std::endl;
             break;
+
         case RewardType::ITEM:
             std::cout << "获得道具: " << reward.description << std::endl;
             break;
     }
 }
+
+
 
 RewardOption RewardSystem_GenerateRandomCardReward() {
     // 从所有卡牌中随机取一张作为奖励
