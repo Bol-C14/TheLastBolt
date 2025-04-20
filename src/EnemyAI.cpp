9d2ccd7adbd703f38f85a83aea17dc4433522757
lxyaa1 <117246711+lxyaa1@users.noreply.github.com>
2025-04-20 17:31:51 +0800

    基本功能

--- src/EnemyAI.cpp
+++ src/EnemyAI.cpp
@@ -5,26 +5,38 @@
 #include <random>
 
 using json = nlohmann::json;
-
-void EnemyState::Enter(){
-
-}
-void EnemyState::Exit(){
-    
-}
+class EnemyState{
+    protected:
+    void virtual Enter();
+    void virtual Exit();
+};
+class EnemyBuffState;
+class EnemyAttackState;
+class EnemyDefendState;
+class EnemyFearState;
 void EnemyStateMachine:: Initialize(EnemyState state){
-
+    this->currentState = EnemyBuffState;
+    this->currentState->Enter();
 }
 void EnemyStateMachine::ChangeState(EnemyState nextState){
-    
+    this->currentState->Exit();
+    this->currentState = nextState;
+    this->currentState->Enter();
 }
-
-void EnemyAI::UpdateAI(Enemy &enemy, BattleState &battle){
+void EnemyStateMachine::UpdateState(const BattleState &battle){
+    int maxhp = battle.currentEnemy.maxhp;
+    if(battle.currentEnemy.hp>maxhp/2&&battle.currentEnemy.hp<maxhp){
+        this->ChangeState(EnemyAttackState);
+    }else if()
+        
+};
+class EnemyBuffState{
 
 }
-ActionType EnemyAI:: GetNextAction(const Enemy &enemy){
+class EnemyAttackState;
+class EnemyDefendState;
+class EnemyFearState;
 
-}
 
 // 从文件加载指定敌人数据
 bool EnemyAI_LoadEnemy(const std::string &filePath, const std::string &enemyId, Enemy &enemy) {
@@ -48,6 +60,7 @@ bool EnemyAI_LoadEnemy(const std::string &filePath, const std::string &enemyId, 
                     ea.intent = act.at("intent").get<std::string>();
                     enemy.nextActions.push_back(ea);
                 }
+                enemy.stateMachine.Initialize(EnemyBuffState()); // 初始化状态机
                 return true;
             }
         }
@@ -63,11 +76,14 @@ void EnemyAI_Update(Enemy &enemy, const BattleState &battle) {
     // TODO: 可以根据battle状态和enemy.hp调整AI策略
     // 当前简单随机选择一个预定义动作
     if (enemy.nextActions.empty()) return;
-    std::mt19937 rng(std::random_device{}());
+    /*std::mt19937 rng(std::random_device{}());
     std::uniform_int_distribution<size_t> dist(0, enemy.nextActions.size() - 1);
     size_t idx = dist(rng);
     EnemyAction chosen = enemy.nextActions[idx];
     enemy.nextActions.clear();
-    enemy.nextActions.push_back(chosen);
+    enemy.nextActions.push_back(chosen);*/
+    enemy.stateMachine.UpdateState(battle);
+
+    EnemyAction chosen;
     std::cout << "敌人选择行动: " << chosen.intent << std::endl;
 }
