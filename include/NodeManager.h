48d42f00dc72afb2a430cb62858a4bb08f17e30d
daoshenshiye <1670834028@qq.com>
2025-04-21 00:20:26 +0800

    Update NodeManager.h

--- include/NodeManager.h
+++ include/NodeManager.h
@@ -19,7 +19,6 @@ public:
      * TODO: 实现JSON解析，填充nodes数组
      */
     bool initNode(Node &ctx);
-
     /**
      * 根据ID查找节点
      * @param ctx 节点管理器上下文
@@ -28,8 +27,7 @@ public:
      * 
      * TODO: 实现节点查找逻辑
      */
-    const Node& NodeManager_GetNodeById(const Node &ctx, int id);
-
+    const Node& NodeManager_GetNodeById(const Node& ctx, int id);
     /**
      * 获取某类型的所有节点
      * @param ctx 节点管理器上下文
@@ -39,7 +37,6 @@ public:
      * TODO: 实现按类型筛选节点
      */
     std::vector<Node> NodeManager_GetNodesByType(const Node &ctx, NodeType type);
-
     /**
      * 节点是否存在
      * @param ctx 节点管理器上下文
@@ -49,12 +46,10 @@ public:
      * TODO: 实现节点存在性检查
      */
     bool NodeManager_NodeExists(const Node &ctx, int id);
-
 private:
     MapSystem mapSystem; // 地图系统
     //RewardSystem rewardSystem; // 奖励系统
     //战斗子系统
 
 };
-
 #endif // NODEMANAGER_H
